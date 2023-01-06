fd33721da8ba62cb809dbb9a08e54febd0595821
nathanatkinson22 <smplifenathan@gmail.com>
2023-01-05 20:28:50 -0700

    used correct code for copyright

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ let dateFooter = document.querySelector('#copyright');
 let lastUpdated = document.querySelector('#last-updated');
 let currentYear = new Date().getFullYear();
 let updatedDate = new Date(document.lastModified).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric", hour:"numeric", minute:"numeric"})
-dateFooter.innerHTML = `© ${currentYear}`;
+dateFooter.innerHTML = `&copy; ${currentYear}`;
 lastUpdated.innerHTML = `<p>Last Updated: ${updatedDate}`;
 
 // Dark mode toggle
