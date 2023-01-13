b625917eeff3ecd86991b8ab9ee3ecd2dffe66b1
nathanatkinson22 <smplifenathan@gmail.com>
2023-01-12 17:02:03 -0700

    fixed broken assets

---
+++ lesson2/scripts/script.js
@@ -0,0 +1,20 @@
+console.log('Hello world!');
+
+let dateFooter = document.querySelector('#copyright');
+let lastUpdated = document.querySelector('#last-updated');
+let currentYear = new Date().getFullYear();
+let updatedDate = new Date(document.lastModified).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric", hour:"numeric", minute:"numeric"})
+dateFooter.innerHTML = `&copy; ${currentYear}`;
+lastUpdated.innerHTML = `<p>Last Updated: ${updatedDate}`;
+
+// Dark mode toggle
+const darkButton = document.querySelector('#dark-mode');
+darkButton.addEventListener('click', function() {
+    document.body.classList.toggle("dark-theme");
+    let sunIcon = this.querySelector('.fa-sun');
+    if (sunIcon.classList.contains('fa-regular')) {
+        darkButton.innerHTML = `<i class="fa-solid fa-sun"></i> Turn the lights on`;
+    } else {
+        darkButton.innerHTML = `<i class="fa-regular fa-sun"></i> Turn the lights off`;
+    }
+});
