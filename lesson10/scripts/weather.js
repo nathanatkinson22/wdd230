78f9b1b973cce8b5e3017e35bb98a08a1a362fbf
nathanatkinson22 <smplifenathan@gmail.com>
2023-03-09 19:18:40 -0700

    weather assignment

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,34 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=fairbanks&units=imperial&appid=baab5d803c6942ee7863c1184d688d72`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+            console.log(data); // this is for testing the call
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(weatherdata) {
+    console.log(currentTemp)
+    currentTemp.innerHTML = `<strong>${weatherdata.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.com/img/w/${weatherdata.weather[0].icon}.png`;
+    const desc = weatherdata.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
